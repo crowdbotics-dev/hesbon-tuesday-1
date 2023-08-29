3949e3a0e02b2bcd2cc71e258795f95fd1b5b27f
Crowdbotics <engineering@crowdbotics.com>
2023-08-29 13:23:54 +0000

    Update store.

--- store/newConnector/api.js
+++ store/newConnector/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 import {
-  NEW_CONNECTOR_2_USERNAME,
-  NEW_CONNECTOR_2_PASSWORD
+  NEW_CONNECTOR_3_USERNAME,
+  NEW_CONNECTOR_3_PASSWORD
 } from "react-native-dotenv"
 const newConnector = axios.create({
   baseURL: "https://cbdash-pr-4833.herokuapp.com/dashboard/app/1/connectors",
   auth: {
-    username: NEW_CONNECTOR_2_USERNAME,
-    password: NEW_CONNECTOR_2_PASSWORD
+    username: NEW_CONNECTOR_3_USERNAME,
+    password: NEW_CONNECTOR_3_PASSWORD
   },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
